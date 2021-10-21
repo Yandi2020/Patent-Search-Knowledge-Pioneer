919080f10bbe71ffb846dcd4a640a5a2cc4a07c7
Yandi Wang <wangyandi0406@gmail.com>
2021-10-21 19:19:05 +0200

    fixed small bug in reducer

--- src/components/store/reducer.js
+++ src/components/store/reducer.js
@@ -6,14 +6,14 @@ const initState = {
 }
 
 const reducer = (state = initState, action) => {
-    if(action.type === 'patent title' | action.type === 'chemical type 1'){
+    if(action.type === 'patent title' || action.type === 'chemical type 1'){
         let newDb = db.filter(item => {
             return item[action.type] === action.content;
         })
-
+        console.log(newDb)
         if(newDb.length === 0){
             return {
-                ...state,
+                database: [],
                 error: 'Sorry, we have not found matched results...'
             }
         }else{
